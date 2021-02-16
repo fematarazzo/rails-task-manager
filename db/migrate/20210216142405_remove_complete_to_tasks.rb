793f44f9a6304ccd3bd83e18e8150a083265b9bb
Felipe Matarazzo <felipemps@protonmail.com>
2021-02-16 16:52:35 -0300

    done\!

---
+++ db/migrate/20210216142405_remove_complete_to_tasks.rb
@@ -0,0 +1,5 @@
+class RemoveCompleteToTasks < ActiveRecord::Migration[6.1]
+  def change
+    remove_column :tasks, :completed
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_16_140734) do
+ActiveRecord::Schema.define(version: 2021_02_16_142446) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
-    t.boolean "completed"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "completed", default: false
   end
 
 end
